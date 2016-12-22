f1cda279879b88c4bb20c359709437a3f9026490
Jonathan Salmon <jonathan.salmon@hotmail.com>
2016-12-22 15:55:00 +0000

    added test web support

--- public_html/index.php
+++ public_html/index.php
@@ -3,11 +3,23 @@
     require_once("../resources/config.php");
      
     require_once(TEMPLATES_PATH . "/header.php");
+
+    require_once '../resources/library/meekrodb.2.3.class.php';
+    DB::$user = 'mash';
+    DB::$password = 'u3XxS7QQ8QhEhB2E5kGhPtqVgqFW';
+    DB::$dbName = 'mash';
+
 ?>
 <div id="container">
     <div id="content">
         <!-- content -->
         <?php
+    	$results = DB::query("SELECT user_id, email FROM users");
+    	foreach ($results as $row) {
+		echo "ID: " . $row['user_id'] . "<br>";
+		echo "Email: " . $row['email'] . "<br>";
+		echo "<br>";
+	}
         phpinfo();
         ?>
     </div>
