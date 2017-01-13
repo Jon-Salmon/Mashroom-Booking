b69be0701394f822e21e7c847daa0841d2ec25e6
Jonathan Salmon <jonathan.salmon@hotmail.co.uk>
2017-01-13 19:22:40 +0000

    Fixed another wrong reference

--- public_html/mash/index.php
+++ public_html/mash/index.php
@@ -17,9 +17,7 @@ require_once('../../resources/globalNoUser.php');
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     
-    <script src="<?php echo HTTP_ROOT?>js/induct_ajax.js"></script>
-    
-    <title>Mash Room</title>
+    <title>MASH Room</title>
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
